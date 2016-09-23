6fd18de53e31df6a159426fb0b0eb509ee811609
xuzepei <xuzepei@smalltreemedia.com>
2016-09-23 11:27:06 +0800

    Initial Commit

---
+++ UKRadio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UKRadio
+//
+//  Created by xuzepei on 16/9/23.
+//  Copyright © 2016年 xuzepei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
